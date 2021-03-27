30a78ca5f8660201a8d4b9bad6b4f1337f10017a
Srirammkm <35733243+Srirammkm@users.noreply.github.com>
2021-03-27 13:54:15 +0530

    Update app.py

--- k8s-deploy/app.py
+++ k8s-deploy/app.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 mydb = mysql.connector.connect(
   #host in which DB is running. In this case IKS cluste.
-  host="169.57.56.131",
+  host="46.137.195.182",
   port=30002,
   user="root",
   password="root",
